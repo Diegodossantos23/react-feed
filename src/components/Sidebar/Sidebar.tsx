5c5aca684ff9b3be5998dc343515e9aa41a453b8
Diego dos Santos <diego@DESKTOP-0IRUF00.localdomain>
2022-06-17 17:23:39 -0300

    fix: finished sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,27 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export const Sidebar = () => {
     return(
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src={"https://media-exp1.licdn.com/dms/image/C4D16AQEhNOdSRk5W1w/profile-displaybackgroundimage-shrink_350_1400/0/1617631715305?e=1660780800&v=beta&t=dQ2hzb4CoTPCTaV5CqCivOexZDg0-WPvzPSRmHIuXMA"}/>
+            <img 
+                className={styles.cover} 
+                src={"https://media-exp1.licdn.com/dms/image/C4D16AQEhNOdSRk5W1w/profile-displaybackgroundimage-shrink_350_1400/0/1617631715305?e=1660780800&v=beta&t=dQ2hzb4CoTPCTaV5CqCivOexZDg0-WPvzPSRmHIuXMA"}
+            />
+            
             <div className={styles.profile}>
-                <strong>Diego Rosa</strong>
-                <span>Frontent Developer</span>
+                <img className={styles.avatar} src="https://github.com/Diegodossantos23.png"/>
+                <strong className={styles.name}>Diego Rosa</strong>
+                <span className={styles.role}>Front-end Developer</span>
             </div>
 
             <footer className={styles.footer}>
-                <a className={styles.link} href="#">Edit your profile</a>
+                <a 
+                    className={styles.link} 
+                    href="#">
+                        <PencilLine size={20}/>
+                        Edit your profile
+                </a>
             </footer>
         </aside>
     )
